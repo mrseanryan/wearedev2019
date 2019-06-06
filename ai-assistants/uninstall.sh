13092a1c28b25378f388d5dabd71d3a3b407bf10
Sean Ryan <sean.ryan@mendix.com>
2019-06-06 13:30:44 +0200

    Add uninstall script

---
+++ ai-assistants/uninstall.sh
@@ -0,0 +1,18 @@
+pip3 uninstall protobuf
+pip3 uninstall numpy
+pip3 uninstall tensorflow-estimator
+pip3 uninstall keras-preprocessing
+pip3 uninstall tensorboard
+pip3 uninstall keras-applications
+pip3 uninstall astor
+pip3 uninstall gast
+pip3 uninstall six
+pip3 uninstall absl-py
+pip3 uninstall grpcio
+pip3 uninstall wheel
+pip3 uninstall termcolor
+
+pip3 uninstall tensorflow
+pip3 uninstall scipy
+pip3 uninstall rasa-x
+
