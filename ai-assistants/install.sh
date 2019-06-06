ab40d11f196318feb522d3dc05b5d1f0cc05f357
Sean Ryan <sean.ryan@mendix.com>
2019-06-06 11:15:55 +0200

    Initial version with install script

---
+++ ai-assistants/install.sh
@@ -0,0 +1,5 @@
+sudo apt update
+
+sudo apt install python-pip
+
+pip3 install rasa-x --extra-index-url ​ https://pypi.rasa.com/simple
---
+++ ai-assistants/machine-learning-notes.md
@@ -0,0 +1,4 @@
+# Building AI assistants that scale using machine learning and OSS tools
+
+Justina Petraityte - company: RASA
+
